519ef6bff27e74e0cd55caf71f613bbc5e7af594
RobinAlfengård <robin.alfengard@hotmail.com>
2023-05-23 15:44:00 +0200

    repository

--- src/main/java/com/example/BookClub/Controllers/UserController.java
+++ src/main/java/com/example/BookClub/Controllers/UserController.java
@@ -2,6 +2,7 @@ package com.example.BookClub.Controllers;
 
 import com.example.BookClub.Entities.User;
 import com.example.BookClub.Repositories.UserRepository;
+import com.example.BookClub.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,11 +10,15 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/users")
 public class UserController {
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    UserService userService;
 
     @GetMapping("/list")
     public ResponseEntity<List<User>> getUsers(){
@@ -23,6 +28,11 @@ public class UserController {
     @PostMapping("/add")
     public ResponseEntity<User> addUser(@RequestBody User user){
        return ResponseEntity.ok(userRepository.save(user));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id){
+        return userService.deleteById(id);
     }
 
 
--- src/main/java/com/example/BookClub/Services/UserService.java
+++ src/main/java/com/example/BookClub/Services/UserService.java
@@ -1,12 +1,24 @@
 package com.example.BookClub.Services;
 
+import com.example.BookClub.Entities.User;
+import com.example.BookClub.Repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
+    private final UserRepository userRepository;
 
+    public ResponseEntity<String> deleteById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("User not found with id: " + id));
+        userRepository.deleteById(id);
+        return ResponseEntity.ok( "Member with id " + id + " was successfully deleted.");
+    }
 
 }
