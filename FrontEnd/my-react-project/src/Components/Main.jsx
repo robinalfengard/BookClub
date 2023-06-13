56374ac53c5090ab0c4e8c2bd4cf86b113473997
RobinAlfengård <robin.alfengard@hotmail.com>
2023-06-13 10:39:05 +0200

    updated can now add and render attendees

--- FrontEnd/my-react-project/src/Components/Main.jsx
+++ FrontEnd/my-react-project/src/Components/Main.jsx
@@ -5,54 +5,42 @@ import axios from "axios";
 
 const Main=()=>{
     const[search, setSearch]=useState("");
-    const [bookData, setData]=useState([]);
-
-      const handleClick = async () => {
-        try {
-            await axios.post()
-            
-        } catch (error) {
-            console.log(error)
-            
-        }
-      }
-    
+    const [bookData, setData]=useState([]);    
  
 
     const searchBook=(event)=>{
         if(event.key==="Enter")
         {   
             //INSERT google api key where #GoogleKey is printed below
-            axios.get()
+            axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyBHSJTe7_TJNwsGj83-pNzPlfZ8nJG2GJo'+'&maxResults=12')
             .then(res=>setData(res.data.items))
             .catch(err=>console.log(err))
         }
     }
 
+    const handleClick = async () => {
+        axios.get()
+        .then(res=>setData(res.data.items))
+        .catch(err=>console.log(err))
+      };
+
     return(
         <>
-
-
-
             <div className="search">
                     <input type="text" placeholder="Book Name"
                     value={search} onChange={e=>setSearch(e.target.value)}
                     onKeyDown={searchBook}
                     />
                    
-                    <button>Search for Books</button>
+                    <button className="" onClick={handleClick}>Search for Books</button>
                 </div>
             <div className="container">
                 {
                     <Card book={bookData}/>
-
                 }
                 
-                
-   
             </div>
 
-        
         </>
     )
 }
